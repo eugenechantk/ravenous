0743beca980947536537f099828d9e51e7ff362d
Eugene Chan <eugenechan@Eugenes-MacBook-Pro.local>
2020-11-16 23:38:08 +0800

    complete basic layout of the app

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './SearchBar.css'
 
-let sortByOptions = {
+const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
@@ -11,7 +11,7 @@ class SearchBar extends React.Component{
     renderSortByOptions(){
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            <li key={sortByOptionValue}>{sortByOption}</li>;
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
         })
     }
     render(){
@@ -19,7 +19,7 @@ class SearchBar extends React.Component{
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions}
+                        {this.renderSortByOptions()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
