fed1df5d5639c25596c0a11f32d38075f6863b87
Eugene Chan <eugenechan@Eugenes-MacBook-Pro.local>
2020-11-20 23:08:09 +0800

    modularized business cards to display in a business list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,24 @@ import './App.css'
 import SearchBar from '../SearchBar/SearchBar'
 import BusinessList from '../BusinessList/BusinessList'
 
+let business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+}
+
+const businesses = [business,business,business,business,business,business]
+
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
+      <div className="App" businesses={businesses}>
         <h1>ravenous</h1>
         <SearchBar />
         <BusinessList /> 
